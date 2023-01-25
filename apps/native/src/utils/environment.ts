451ea590162ee559d10357ef7ec0e091a3a54dd6
Igor Szukalo <igor@makersden.io>
2023-01-25 12:22:26 +0100

    feat: remove comments move parser

--- apps/native/src/utils/environment.ts
+++ apps/native/src/utils/environment.ts
@@ -1,16 +1,15 @@
 import Constants from "expo-constants";
 import { z } from "zod";
 
-// @TODO validate envs (zod?) and move them somewhere
-export const environment = Object.freeze({
-  API_URL: Constants.manifest?.extra?.API_URL,
-  EXAMPLE_ESTIMATION_SECRET:
-    Constants.manifest?.extra?.EXAMPLE_ESTIMATION_SECRET,
-});
-
 const validateEnvVars = z.object({
   EXAMPLE_ESTIMATION_SECRET: z.string().min(1),
   API_URL: z.string().url(),
 });
 
-validateEnvVars.parse(environment);
+export const environment = validateEnvVars.parse(
+  Object.freeze({
+    API_URL: Constants.manifest?.extra?.API_URL,
+    EXAMPLE_ESTIMATION_SECRET:
+      Constants.manifest?.extra?.EXAMPLE_ESTIMATION_SECRET,
+  })
+);
--- apps/web/src/utils/environment.ts
+++ apps/web/src/utils/environment.ts
@@ -1,17 +1,5 @@
 import { z } from "zod";
 
-// @TODO validate envs (zod?) and move them somewhere
-export const environment = Object.freeze({
-  EXAMPLE_ESTIMATION_SECRET: process.env.NEXT_PUBLIC_EXAMPLE_ESTIMATION_SECRET,
-  GOOGLE_SHEETS_API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
-  STORYBLOK_ACCESS_TOKEN: process.env.STORYBLOK_ACCESS_TOKEN,
-  STORYBLOK_ENVIRONMENT_FOLDER_NAME:
-    process.env.STORYBLOK_ENVIRONMENT_FOLDER_NAME,
-  STORYBLOK_ESTIMATIONS_FOLDER_ID: process.env.STORYBLOK_ESTIMATIONS_FOLDER_ID,
-  STORYBLOK_OAUTH_TOKEN: process.env.STORYBLOK_OAUTH_TOKEN,
-  STORYBLOK_SPACE_ID: process.env.STORYBLOK_SPACE_ID,
-});
-
 const validateEnvVars = z.object({
   EXAMPLE_ESTIMATION_SECRET: z.string().min(1),
   GOOGLE_SHEETS_API_KEY: z.string().min(1),
@@ -22,4 +10,17 @@ const validateEnvVars = z.object({
   STORYBLOK_SPACE_ID: z.string().min(1),
 });
 
-validateEnvVars.parse(environment);
+export const environment = validateEnvVars.parse(
+  Object.freeze({
+    EXAMPLE_ESTIMATION_SECRET:
+      process.env.NEXT_PUBLIC_EXAMPLE_ESTIMATION_SECRET,
+    GOOGLE_SHEETS_API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
+    STORYBLOK_ACCESS_TOKEN: process.env.STORYBLOK_ACCESS_TOKEN,
+    STORYBLOK_ENVIRONMENT_FOLDER_NAME:
+      process.env.STORYBLOK_ENVIRONMENT_FOLDER_NAME,
+    STORYBLOK_ESTIMATIONS_FOLDER_ID:
+      process.env.STORYBLOK_ESTIMATIONS_FOLDER_ID,
+    STORYBLOK_OAUTH_TOKEN: process.env.STORYBLOK_OAUTH_TOKEN,
+    STORYBLOK_SPACE_ID: process.env.STORYBLOK_SPACE_ID,
+  })
+);
