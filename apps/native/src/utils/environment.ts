430f2952698e08442f8a4bdad833d4660b88feda
Igor Szukalo <igor@makersden.io>
2023-01-25 11:56:21 +0100

    feat: zod env validation

--- apps/native/src/utils/environment.ts
+++ apps/native/src/utils/environment.ts
@@ -1,7 +1,16 @@
 import Constants from "expo-constants";
+import { z } from "zod";
 
 // @TODO validate envs (zod?) and move them somewhere
 export const environment = Object.freeze({
   API_URL: Constants.manifest?.extra?.API_URL,
-  EXAMPLE_ESTIMATION_SECRET: Constants.manifest?.extra?.EXAMPLE_ESTIMATION_SECRET,
+  EXAMPLE_ESTIMATION_SECRET:
+    Constants.manifest?.extra?.EXAMPLE_ESTIMATION_SECRET,
 });
+
+const validateEnvVars = z.object({
+  EXAMPLE_ESTIMATION_SECRET: z.string().min(1),
+  API_URL: z.string().url(),
+});
+
+validateEnvVars.parse(environment);
