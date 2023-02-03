9d07b5c5a4e185159a3ade9631322dade739a8dc
Mateusz Ziomek <mateusz.ziomek@makersden.io>
2023-02-03 13:49:14 +0100

    chore: adjust comments

--- apps/native/src/utils/environment/validate.ts
+++ apps/native/src/utils/environment/validate.ts
@@ -5,8 +5,6 @@ export type EnvironmentVariables = z.infer<typeof validateEnvVars>;
 export const validateEnvVars = z.object({
   /**
    * @TODO Add env description
-   * lan IP (for example 192.168.1.20) must be used instead of localhost
-   * probably some config must be added before using on ios https://stackoverflow.com/questions/38418998/react-native-fetch-network-request-failed
    */
   API_URL: z.string().url(),
   /**
