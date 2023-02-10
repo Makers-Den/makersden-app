0320323bf80b7b49a30130d68e56567534dd9aac
Mateusz Ziomek <mateusz.ziomek@makersden.io>
2023-02-10 10:59:05 +0100

    fix: estimation list width jump

--- packages/ui/src/components/ContentWrapper.tsx
+++ packages/ui/src/components/ContentWrapper.tsx
@@ -1,14 +1,22 @@
-import { Box } from "native-base";
+import { Box, useBreakpointValue } from "native-base";
 import React, { ReactNode } from "react";
 
 export type ContentWrapperProps = {
   children: ReactNode;
 };
 
-export const ContentWrapper = ({ children }: ContentWrapperProps) => (
-  <Box minH={"full"} _web={{ minH: "100vh" }} bg="black.200">
-    <Box maxW={"1300px"} mx={"auto"}>
-      {children}
+export const ContentWrapper = ({ children }: ContentWrapperProps) => {
+  const containerStyles = useBreakpointValue({
+    base: {},
+    lg: {
+      maxW: "1300px",
+      mx: "auto",
+    },
+  });
+
+  return (
+    <Box minH={"full"} _web={{ minH: "100vh" }} bg="black.200">
+      <Box {...containerStyles}>{children}</Box>
     </Box>
-  </Box>
-);
+  );
+};
