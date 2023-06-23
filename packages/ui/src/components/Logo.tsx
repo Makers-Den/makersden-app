d640509582503340b9714ad9664849ab65471a0e
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-06-23 14:37:06 +0200

    fix: mobile version crushing

--- packages/ui/src/components/Logo.tsx
+++ packages/ui/src/components/Logo.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import React from "react";
+import { Image, StyleSheet } from "react-native";
 
 export interface LogoProps {
   isDark: boolean;
@@ -7,11 +7,10 @@ export interface LogoProps {
 
 export const Logo = ({ isDark }: LogoProps) => {
   return (
+    // eslint-disable-next-line jsx-a11y/alt-text
     <Image
-      width={200}
-      height={29}
-      alt="Logo"
-      src={
+      style={styles.image}
+      source={
         isDark
           ? require("../../assets/logo-dark.png")
           : require("../../assets/logo.png")
@@ -19,3 +18,10 @@ export const Logo = ({ isDark }: LogoProps) => {
     />
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 29,
+  },
+});
