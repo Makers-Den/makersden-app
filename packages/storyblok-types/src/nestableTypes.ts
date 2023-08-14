b28b550a9430eed23c72d797765c751d7990d7ae
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-08-14 15:36:10 +0200

    fix: type error

--- packages/storyblok-types/src/nestableTypes.ts
+++ packages/storyblok-types/src/nestableTypes.ts
@@ -12,7 +12,10 @@ export type BlockComponent = (props: any) => JSX.Element;
  * The types of components/blocks we support.
  * The are usually in the 'body' field of a page.
  */
-export type ComponentBlockType = "RichTextContent" | "SoWEstimationSection";
+export type ComponentBlockType =
+  | "RichTextContent"
+  | "SoWEstimationSection"
+  | "PageBreak";
 
 export interface RichTextContentContent {
   _uid?: string;
