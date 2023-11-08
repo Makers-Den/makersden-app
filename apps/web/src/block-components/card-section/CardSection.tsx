7ad94f9047d9e8534d4cf53fa011af8cd6b4daf6
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-11-08 12:25:26 +0100

    feat: no wrap on card section

--- apps/web/src/block-components/card-section/CardSection.tsx
+++ apps/web/src/block-components/card-section/CardSection.tsx
@@ -5,14 +5,7 @@ import { Card } from "../card/Card";
 
 export const CardSection = ({ cards }: CardSectionContent) => {
   return (
-    <Flex
-      direction="row"
-      mt="6"
-      mb="-6"
-      mr="-6"
-      flexWrap="wrap"
-      justifyContent="center"
-    >
+    <Flex direction="row" mt="6" mb="-6" mr="-6" justifyContent="center">
       {cards.map((card) => (
         <Box key={card._uid} mr="6" mb="12">
           <Card {...card} />
