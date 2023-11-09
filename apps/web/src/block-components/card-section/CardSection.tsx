37129a7102da9ce964d42bd285a00744a8207591
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-11-09 08:32:42 +0100

    feat: 2 column layout

--- apps/web/src/block-components/card-section/CardSection.tsx
+++ apps/web/src/block-components/card-section/CardSection.tsx
@@ -1,16 +1,39 @@
-import { CardSectionContent } from "@md/storyblok-types";
-import { Box, Flex } from "native-base";
+import { CardContent, CardSectionContent } from "@md/storyblok-types";
+import { Box, HStack, VStack } from "native-base";
 
 import { Card } from "../card/Card";
 
 export const CardSection = ({ cards }: CardSectionContent) => {
+  const createRows = (contents: CardContent[]): CardContent[][] => {
+    const rows: CardContent[][] = [];
+
+    for (let i = 0; i < contents.length; i += 2) {
+      const row: CardContent[] = [];
+      row.push(contents[i]);
+
+      if (i + 1 < contents.length) {
+        row.push(contents[i + 1]);
+      }
+
+      rows.push(row);
+    }
+
+    return rows;
+  };
+
+  const rows = createRows(cards);
+
   return (
-    <Flex direction="row" mt="6" mb="-6" mr="-6" justifyContent="center">
-      {cards.map((card) => (
-        <Box key={card._uid} mr="6" mb="12">
-          <Card {...card} />
-        </Box>
+    <VStack>
+      {rows.map((row) => (
+        <HStack direction="row" mt="6" mb="-6" mr="-6" justifyContent="center">
+          {row.map((card) => (
+            <Box key={card._uid} mr="6" mb="12">
+              <Card {...card} />
+            </Box>
+          ))}
+        </HStack>
       ))}
-    </Flex>
+    </VStack>
   );
 };
